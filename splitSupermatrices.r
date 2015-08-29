d33e70cb959f919c70264cd3d888d6965737212a
NathanWhelan <NathanWhelan@users.noreply.github.com>
2015-08-29 14:55:37 -0500

    Update splitSupermatrices.r
    Now reads phylip alignment and takes gene list in PartitionFinder format. Generally increased user experience.

--- splitSupermatrices.r
+++ splitSupermatrices.r
@@ -16,27 +16,33 @@
 ##########################################################################################################################
 
 
+
 library(Biostrings)
-setwd("~/") ##Working  Directory Change as needed
+setwd("~/postDoc/Scripts/test/") ##Change as needed
 rm(list = ls())
 
-##The file name should be a suprmatrix in fasta format. Must modify as needed
-sequenceData <- read.AAStringSet("FASTA_MATRIX", format="fasta")
+##The file name should be a supermatrix in phylip format. Must modify variable names as needed
+sequenceData<-unmasked(read.AAMultipleAlignment("Dataset1_FullData.phy",format="phylip"))
 
-###This could be in one table and split. This was how I had my start and stop positions fomratted, but may not be easiest for you.
-startVar<-read.table("nosenkoNonRiboStart.txt") #single column with start positions
-endVar<-read.table("nosenkoNonRiboEnd.txt") #singel column with end positions
-nrow(startVar) 
+##Gene list should be in PartitionFinder format (i.e. GENE_NAME = startPostition-stopPosition;)
+##Semicolon at end of each line in gene list is not necessary
+table<-read.table("DataSet1_GeneList_FullData.txt")
+list<-data.frame(do.call('rbind', strsplit(as.character(table$V3),split="-",fixed=TRUE)))
+list$X2<-gsub(";","",list$X2)
+
+startVar<-as.data.frame(list$X1)
+endVar<-as.data.frame(list$X2)
 numberRows<-as.integer(nrow(startVar))
 
-#This for loop will go through and split a supermatrix as specified by the start and stop rows files
-#A single alignment in fasta format will be created for each gene or partition
+#This for loop will go through and split a supermatrix as specified by gene list
+#A single alignment in fasta format will be created for each gene/partition
 for(n in 1:numberRows){
-subset <- AAStringSet(sequenceData, start=startVar[n,], end=endVar[n,])
+subset <- AAStringSet(sequenceData, start=as.integer(as.vector(startVar[n,1])), end=as.integer(as.vector(endVar[n,1])))
 LINE1=toString(n)
 NAME=paste(LINE1,".fas",sep="",collapse=NULL)
 write.XStringSet(x=subset, filepath = NAME, format="fasta") #This will give a unique name
 print(n)
 print(subset)
 }
+
 
